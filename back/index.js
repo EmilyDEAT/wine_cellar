b681449b039139ae33521e77696e5c03352714e8
Emily Déat <emily.deat@gmail.com>
2020-06-06 10:59:24 +0200

    Add route on server

--- back/index.js
+++ back/index.js
@@ -7,7 +7,7 @@ const port = 8000
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.get("/wines", (req, res) => {
+app.get("/allWines", (req, res) => {
   const sql = 'SELECT * FROM wine'
   connection.query(sql, (err, results) => {
     if (err) {
@@ -24,6 +24,31 @@ app.post("/addWines", (req, res) => {
   connection.query(sql,[formData], (err, results) => {
     if (err) {
       res.status(500).send("Erreur lors de l'ajout d'un vin")
+    } else {
+      res.sendStatus(200)
+    }
+  })
+})
+
+app.put("/updateWines/:id", (req, res) => {
+  const idWine = req.params.id
+  const formData = req.body
+  const sql = 'UPDATE wine SET ? WHERE ?'
+  connection.query(sql,[formData, idWine], (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur lors de la modification d'un vin")
+    } else {
+      res.sendStatus(200)
+    }
+  })
+})
+
+app.delete("/deleteWines/:id", (req, res) => {
+  const idWine = req.params.id
+  const sql = 'DELETE FROM wine WHERE id = ?'
+  connection.query(sql,[idWine], (err, results) => {
+    if (err) {
+      res.status(500).send("Erreur lors de la suppression d'un vin")
     } else {
       res.sendStatus(200)
     }
